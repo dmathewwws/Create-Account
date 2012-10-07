2500a339651e3596bda60a6658e18662ab14efab
dannylm <dannyl.mathews@gmail.com>
2012-10-07 13:57:28 -0700

    Initial Commit

---
+++ Create Account/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Create Account
+//
+//  Created by Daniel Mathews on 2012-10-07.
+//  Copyright (c) 2012 com.theplayapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
